343cfe3c9902290228763fd493c8a40419ce9c88
Gelo-dev12 <evatadan1259@gmail.com>
2025-07-19 21:39:14 +0800

    2.5 fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,23 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginNext from "eslint-plugin-next";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js, next: pluginNext },
+    extends: ["js/recommended", "next/core-web-vitals"]
+  },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
 ]);
